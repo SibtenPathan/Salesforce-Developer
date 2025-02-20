d9ba32140c840196775e456f2b14e1c9d396142a
SibtenPathan <sibtenkhan6789@gmail.com>
2025-02-20 16:19:33 +0530

    AURA task1 and 2 without pagination

--- force-app/main/default/aura/auraTask2/auraTask2Controller.js
+++ force-app/main/default/aura/auraTask2/auraTask2Controller.js
@@ -1,47 +1,10 @@
 ({
-    getOptions: function (component, event, helper) {
-        console.log('getOptions called');
-
-        var action = component.get("c.getAccounts");
-        action.setCallback(this, function (response) {
-            var state = response.getState();
-            console.log('state : ', state);
-
-            if (state === "SUCCESS") {
-                var accounts = response.getReturnValue();
-                var formattedAccounts = accounts.map(function (account) {
-                    return {
-                        label: account.Name,
-                        value: account.Id
-                    };
-                });
-                component.set("v.options", formattedAccounts);
-            } else {
-                console.error("Error fetching accounts: " + response.getError());
-            }
-        });
-        $A.enqueueAction(action);
-    }, handleChange: function (component, event, helper) {
-        console.log('handleChange called');
-        var selectedAccountId = event.getSource().get("v.value")
-        console.log('selectedAccountId : ', selectedAccountId);
-
-        var action = component.get("c.getContactsForAccount");
-        action.setParams({
-            accountId: selectedAccountId
-        });
-        action.setCallback(this, function (response) {
-            var state = response.getState();
-            console.log('state : ', state);
-
-            if (state === "SUCCESS") {
-                component.set("v.data", response.getReturnValue());
-                component.set("v.columns", [{ label: 'Name', fieldName: 'Name', type: 'text' }, { label: 'Email', fieldName: 'Email', type: 'email' }, { label: 'Phone', fieldName: 'Phone', type: 'phone' }]);
-            } else {
-                console.error("Error fetching contacts: " + response.getError());
-            }
-        });
-        $A.enqueueAction(action);
+    doInit : function(component, event, helper) {
+        helper.getAccounts(component);
+    },
+    handleChange : function(component, event, helper) {
+        var selectedAccountId = event.getSource().get("v.value");
+        console.log("Selected Account Id: " + selectedAccountId);
+        helper.getContacts(component, selectedAccountId);
     }
-}
-)
+})
--- force-app/main/default/aura/auraTask2/auraTask2Helper.js
+++ force-app/main/default/aura/auraTask2/auraTask2Helper.js
@@ -1,5 +1,34 @@
 ({
-    helperMethod : function() {
-
+    getAccounts : function(component) {
+        var action = component.get("c.getAccounts");
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var formatedAccounts = response.getReturnValue().map(function(account) {
+                    return {
+                        label: account.Name,
+                        value: account.Id
+                    };
+                }
+                );
+                component.set("v.accounts", formatedAccounts);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    getContacts : function(component, accountId) {
+        var action = component.get("c.getContactsForAccount");
+        action.setParams({
+            accountId: accountId
+        });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.data", response.getReturnValue());
+                var columns = [{label: 'Name', fieldName: 'Name', type: 'text'}, {label: 'Email', fieldName: 'Email', type: 'email'}, {label: 'Phone', fieldName: 'Phone', type: 'phone'}];
+                component.set("v.columns", columns);
+            }
+        });
+        $A.enqueueAction(action);
     }
 })
